85020067f7d5191f4465711613eb5cf0b2a89011
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2022-02-21 09:46:12 +0530

    test

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_instance" "webserver" {
       "git clone https://github.com/givemyresume/auto_deploy.git",
       "cd auto_deploy",
       "echo 'FAUNA_DB_KEY=${var.FAUNA_DB_KEY}\nGITHUB_TOKEN=${var.GITHUB_TOKEN}\nAPI_URL=${var.API_URL}' > .env",
-      "sleep 5m && sudo docker-compose -p resumebuilder up | tee ./run.log"
+      "sudo docker ps; sudo docker images; sudo docker-compose version"
     ]
   }
 }
