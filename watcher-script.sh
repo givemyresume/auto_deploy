521b5d925a28f6b9fdf7f4dbb1a2c6e4de145840
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2022-02-22 01:41:00 +0530

    test

--- watcher-script.sh
+++ watcher-script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 sudo docker-compose -f /home/ubuntu/auto_deploy/docker-compose.yml down
-sudo docker-compose -f /home/ubuntu/auto_deploy/docker-compose.yml up -d
+sudo docker-compose rm -f
+sudo docker system prune -a
+sudo docker-compose -f /home/ubuntu/auto_deploy/docker-compose.yml up --force-recreate --build -d
