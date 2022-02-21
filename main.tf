0d4d1179f6a3da35138f9ce30fd09e7c0f3b573d
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2022-02-21 09:08:10 +0530

    trying dc command

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_instance" "webserver" {
       "git clone https://github.com/givemyresume/auto_deploy.git",
       "cd auto_deploy",
       "echo 'FAUNA_DB_KEY=${var.FAUNA_DB_KEY}\nGITHUB_TOKEN=${var.GITHUB_TOKEN}\nAPI_URL=${var.API_URL}' > .env",
-      "sudo bash run_server.sh"
+      "sudo dc -p resumebuilder up | tee ./run.log"
     ]
   }
 }
--- run_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/auto_deploy
-docker-compose -p resumebuilder up | tee ./run.log
