8e9bd25d835d6e6dee5c5e84f0efbaccbca6d9fa
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2022-02-21 08:08:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,6 +68,7 @@ resource "aws_instance" "webserver" {
 
   provisioner "remote-exec" {
     inline = [
+      "#!/bin/bash",
       "git clone https://github.com/givemyresume/auto_deploy.git",
       "cd auto_deploy",
       "echo 'FAUNA_DB_KEY=${var.FAUNA_DB_KEY}\nGITHUB_TOKEN=${var.GITHUB_TOKEN}\nAPI_URL=${var.API_URL}' > .env",
