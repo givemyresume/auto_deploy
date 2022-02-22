8253c56f49f5a84cc01a46fc0685852b2895db26
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2022-02-22 19:02:05 +0530

    added app1 & api1 ssl

--- run_later.sh
+++ run_later.sh
@@ -3,6 +3,9 @@
 # commands to run after everything is provisioned
 sudo certbot --nginx -d app.givemyresume.tech -n -m balasubhayu99@gmail.com --eff-email --agree-tos | tee /home/ubuntu/logs/certbot_app.log
 sudo certbot --nginx -d api.givemyresume.tech -n -m balasubhayu99@gmail.com --eff-email --agree-tos | tee /home/ubuntu/logs/certbot_api.log
+sudo certbot --nginx -d app1.givemyresume.tech -n -m balasubhayu99@gmail.com --eff-email --agree-tos | tee /home/ubuntu/logs/certbot_app1.log
+sudo certbot --nginx -d api1.givemyresume.tech -n -m balasubhayu99@gmail.com --eff-email --agree-tos | tee /home/ubuntu/logs/certbot_api1.log
+
 nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/api >> /home/ubuntu/api.log &
 nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/website >> /home/ubuntu/website.log &
 nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/api/givemyresume.github.io >> /home/ubuntu/givemyresume.log &
