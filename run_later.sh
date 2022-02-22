0ede26d1e51b803827cbcb01c31a7e41afbc2e98
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2022-02-22 13:38:33 +0530

    .

--- run_later.sh
+++ run_later.sh
@@ -3,6 +3,6 @@
 # commands to run after everything is provisioned
 sudo certbot --nginx -d app.givemyresume.tech -n -m balasubhayu99@gmail.com --eff-email --agree-tos | tee /home/ubuntu/logs/certbot_app.log
 sudo certbot --nginx -d api.givemyresume.tech -n -m balasubhayu99@gmail.com --eff-email --agree-tos | tee /home/ubuntu/logs/certbot_api.log
-nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/api &
-nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/website &
-nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/api/givemyresume.github.io &
+nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/api >> /home/ubuntu/api.log &
+nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/website >> /home/ubuntu/website.log &
+nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/api/givemyresume.github.io >> /home/ubuntu/givemyresume.log &
