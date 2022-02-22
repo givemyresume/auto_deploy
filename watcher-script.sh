019e4394c3a3da0c46c6454c1530ad2f67c1e158
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2022-02-22 20:49:36 +0530

    changed watcher_script

--- watcher-script.sh
+++ watcher-script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd /home/ubuntu/auto_deploy 
-sudo docker-compose down
-sudo docker-compose rm -f
-sudo docker-compose up --force-recreate --build -d
+sudo docker-compose build --no-cache
+sudo docker-compose up -d
 cd -
