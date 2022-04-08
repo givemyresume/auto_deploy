50912d914669395790e0be46986daaa2e137cd8a
Mohit Garg <mohit.garg@fiftyfivetech.io>
2022-04-08 23:22:06 +0530

    removed subdomains with 0

--- run_later.sh
+++ run_later.sh
@@ -4,8 +4,6 @@
 # add ssl for all domains
 sudo certbot --nginx -d app.givemyresume.tech -n -m balasubhayu99@gmail.com --eff-email --agree-tos | tee /home/ubuntu/logs/certbot_app.log
 sudo certbot --nginx -d api.givemyresume.tech -n -m balasubhayu99@gmail.com --eff-email --agree-tos | tee /home/ubuntu/logs/certbot_api.log
-sudo certbot --nginx -d app0.givemyresume.tech -n -m balasubhayu99@gmail.com --eff-email --agree-tos | tee /home/ubuntu/logs/certbot_app0.log
-sudo certbot --nginx -d api0.givemyresume.tech -n -m balasubhayu99@gmail.com --eff-email --agree-tos | tee /home/ubuntu/logs/certbot_api0.log
 
 # watch all the below repos for changes && pull
 nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/api &
