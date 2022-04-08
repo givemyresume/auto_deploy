7f8e8310f81726601c2abd810d3e04c2ecf5eb9c
Mohit Garg <mohit.garg@fiftyfivetech.io>
2022-04-09 00:02:22 +0530

    git-repo-watcher fix

--- run_later.sh
+++ run_later.sh
@@ -6,6 +6,6 @@ sudo certbot --nginx -d app.givemyresume.tech -n -m balasubhayu99@gmail.com --ef
 sudo certbot --nginx -d api.givemyresume.tech -n -m balasubhayu99@gmail.com --eff-email --agree-tos | tee /home/ubuntu/logs/certbot_api.log
 
 # watch all the below repos for changes && pull
-nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/api &
-nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/website &
-nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/api/givemyresume.github.io &
+nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/auto_deploy/api &
+nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/auto_deploy/website &
+nohup /home/ubuntu/auto_deploy/git-repo-watcher -d /home/ubuntu/auto_deploy/api/givemyresume.github.io &
--- watcher-script.sh
+++ watcher-script.sh
@@ -2,6 +2,5 @@
 
 cd /home/ubuntu/auto_deploy 
 sudo docker-compose down
-sudo docker-compose build --no-cache
 sudo docker-compose up -d
 cd -
